785390390877eccdee47c21fe7e86cd8dbb91194
Vladyslav M <68401718+vlmarch@users.noreply.github.com>
2021-11-19 21:45:33 +0100

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -5,23 +5,23 @@ function setup() {
   x = width / 2;
   y = height / 2;
   radius = 175;
-  segments = 100;
+  segments = 200;
   factor = -26;
   frameRate(13);
-  
+
 }
 
 function draw() {
   background(255);
   stroke(50);
   fill(255);
-  ellipse(x, y, radius*2, radius*2);
-  
-  for (var i = 0; i < segments+1; i++){
-    line(radius*sin(i*2*PI/segments) + x,
-         radius*cos(i*2*PI/segments) + y,
-         radius*sin((i*factor)*PI/13/segments) + x,
-         radius*cos((i*factor)*PI/13/segments) + y)
+  ellipse(x, y, radius * 2, radius * 2);
+
+  for (var i = 0; i < segments + 1; i++) {
+    line(radius * sin(i * 2 * PI / segments) + x,
+      radius * cos(i * 2 * PI / segments) + y,
+      radius * sin((i * factor) * PI / 13 / segments) + x,
+      radius * cos((i * factor) * PI / 13 / segments) + y)
   }
   factor += 1;
 }
